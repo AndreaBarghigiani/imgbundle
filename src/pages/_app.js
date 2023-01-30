ee7aec79c245f0071faf9b2e8b94a3551cb9d190
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-01-30 05:22:03 +0100

    feat: first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Josefin_Sans } from "@next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  variable: "--font-josefin",
+  display: "optional",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={josefin.variable}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
