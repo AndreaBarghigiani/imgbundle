88ff6890de9105f85e28bab9ac4fe2b456b14a89
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-02-10 11:26:41 +0100

    feat: add wip banner

--- src/App.jsx
+++ src/App.jsx
@@ -30,12 +30,32 @@ function App() {
   };
   return (
     <>
+      <div className="mx-auto mt-10 max-w-xl rounded-lg border border-slate-700 bg-slate-800 p-4 text-center text-sm font-thin text-slate-400 shadow">
+        <p>
+          If you read this please understand that the app{" "}
+          <strong>is not yet finished</strong>.
+          <br />
+          <em className="text-xs">
+            It already works and have some animations, but I want to add more.
+            Add your ideas to{" "}
+            <a
+              className="not-italic underline"
+              href="https://github.com/AndreaBarghigiani/imgbundle/issues"
+              target="_blank"
+            >
+              GitHub
+            </a>
+            .
+          </em>
+        </p>
+      </div>
       <Avatar />
       <main className="container relative mx-auto">
         <header className="my-32 text-center">
           <h1 className=" font-headings text-7xl font-bold">ImgBundle</h1>
-          <p className="mt-4 text-sm text-slate-400">
-            Find your images and bundle in a single <code>.zip</code> archive.
+          <p className="mt-4 text-slate-400">
+            Revolutionize the way you search and download images, in a{" "}
+            <code>.zip</code>
           </p>
         </header>
         <section className="my-32">
